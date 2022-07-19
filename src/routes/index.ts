25c4713c7f68e18c554bd2c09252eb2d9dc48845
Odonno <bottiau.david@laposte.net>
2022-07-19 18:34:18 +0200

    🐛 exclude user agents from adding invocation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { dev } from '$app/env';
 import { addInvocation } from '$lib/db';
 import type { Header } from '$lib/models';
 
-const currentReferers = dev
+const excludedReferers = dev
 	? ['http://127.0.0.1:5173/']
 	: ['https://vercel.com/', 'https://display-headers.vercel.app/'];
+
+const excludedUserAgents = [/Vercelbot/i, /github.com\/sindresorhus\/got/i, /^node-fetch$/i];
 
 export const GET = async ({ request }) => {
 	const headers: Header[] = [];
@@ -14,7 +16,16 @@ export const GET = async ({ request }) => {
 	}
 
 	const refererHeader = headers.find(({ key }) => key.toLowerCase() === 'referer');
-	const shouldAddInvocation = currentReferers.every((referer) => refererHeader?.value !== referer);
+	const isRefererExcluded = excludedReferers.some(
+		(excludedReferer) => refererHeader?.value === excludedReferer
+	);
+
+	const userAgentHeader = headers.find(({ key }) => key.toLowerCase() === 'user-agent');
+	const isUserAgentExcluded = excludedUserAgents.some((excludedUserAgent) =>
+		userAgentHeader?.value.match(excludedUserAgent)
+	);
+
+	const shouldAddInvocation = !isRefererExcluded && !isUserAgentExcluded;
 
 	if (shouldAddInvocation) {
 		await addInvocation(headers);
