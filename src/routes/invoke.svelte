4b49d74e140fdb7f0b9db0866b36c3c50ad092d6
Odonno <bottiau.david@laposte.net>
2022-07-20 11:20:06 +0200

    🚚 move invocation save in /invoke route

---
+++ src/routes/invoke.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import type { Invocation } from '$lib/models';
+	import { deleteAllInvocations, watchInvocations } from '$lib/db';
+	import type { Subscription } from 'rxjs';
+
+	let invocations: Invocation[] = [];
+	let subscription: Subscription | undefined;
+
+	onMount(async () => {
+		subscription = watchInvocations().subscribe((data) => {
+			invocations = data;
+		});
+	});
+
+	onDestroy(() => {
+		subscription?.unsubscribe();
+	});
+
+	const onClearButton = async () => {
+		await deleteAllInvocations(invocations);
+	};
+</script>
+
+<button type="button" on:click={onClearButton}>Clear all invocations</button>
+
+<div style="color: green;">Invoke successfully executed!</div>
+
+<ul>
+	{#each invocations as invocation (invocation.id)}
+		<li>
+			<a href={`/invocations/${invocation.id}`}>
+				<b>{new Date(invocation.timestamp).toLocaleString()}</b>
+				<div>{invocation.headers.length} headers</div>
+			</a>
+		</li>
+	{/each}
+</ul>
