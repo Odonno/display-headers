3823d624897f35e2664014c7beedea06672496ca
Odonno <bottiau.david@laposte.net>
2022-07-20 11:44:14 +0200

    ♻️ set firebase config with environment variables

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,12 +1,8 @@
 import { initializeApp } from 'firebase/app';
+import { variables } from './variables';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyAo_Jtk55kcxoA9V5o1pDsVt6LPiL2FM3c',
-	authDomain: 'display-headers.firebaseapp.com',
-	projectId: 'display-headers',
-	storageBucket: 'display-headers.appspot.com',
-	messagingSenderId: '866477926715',
-	appId: '1:866477926715:web:f468695c309758c8ee6f1e'
+	...variables.firebase
 };
 
 export const app = initializeApp(firebaseConfig);
